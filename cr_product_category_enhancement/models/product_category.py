ab9afe34689c8832e004df31dbeb3d94590e32b1
dipen <dipen.creyox@gmail.com>
2026-05-19 10:46:11 +0530

    formated with black cmd

--- cr_product_category_enhancement/models/product_category.py
+++ cr_product_category_enhancement/models/product_category.py
@@ -4,11 +4,16 @@
 from odoo import models, api
 from odoo.osv import expression
 
+
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
     @api.model
     def _search(self, domain, offset=0, limit=None, order=None):
-        if self.env.context.get('restrict_categ_evr'):
-            domain = expression.AND([domain or [], [('name', 'in', ['EVR', 'Bank Hours'])]])
-        return super(ProductCategory, self)._search(domain, offset=offset, limit=limit, order=order)
+        if self.env.context.get("restrict_categ_evr"):
+            domain = expression.AND(
+                [domain or [], [("name", "in", ["EVR", "Bank Hours"])]]
+            )
+        return super(ProductCategory, self)._search(
+            domain, offset=offset, limit=limit, order=order
+        )
