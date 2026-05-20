70e7ab789be5ccc08779ab7fdfc16c1cc17ef066
dipen <dipen.creyox@gmail.com>
2026-05-20 13:00:27 +0530

    [UPD] Error message on creating new category

--- cr_product_category_enhancement/models/product_category.py
+++ cr_product_category_enhancement/models/product_category.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Creyox Technologies.
 
-from odoo import models, api
+from odoo import models, api, _
 from odoo.osv import expression
+from odoo.exceptions import UserError
 
 
 class ProductCategory(models.Model):
@@ -17,3 +18,15 @@ class ProductCategory(models.Model):
         return super(ProductCategory, self)._search(
             domain, offset=offset, limit=limit, order=order
         )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        if self.env.context.get("restrict_categ_evr"):
+            for vals in vals_list:
+                if vals.get("name") not in ["EVR", "Bank Hours"]:
+                    raise UserError(
+                        _(
+                            "You should only create for this process the products of evr or bank hour category"
+                        )
+                    )
+        return super(ProductCategory, self).create(vals_list)
